52e66349ecbbea68b98a5b4786d88cd8a522ad23
maldad <canond_7@hotmail.com>
2015-05-17 01:08:42 -0500

    abecedario a ASCII y binario

--- LetraBinaria.java
+++ LetraBinaria.java
@@ -1,25 +1,15 @@
 public class LetraBinaria{
-	
-	/*public static void main (String args[]){
-      String letras ="oso", n;
-      int x=0;
-      for (int i=0; i<letras.length(); i++){
-      x=letras.charAt(i);
-      n=Integer.toBinaryString(x);
-      System.out.println(letras.charAt(i));
-      System.out.println(x);
-      System.out.println(n+"\n");
-      }
-    }//main */
 
     String cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     int ascii;
+    String binario;
 
     public void conversor(){
     	
     	for(int i = 0; i < cadena.length(); i++){
     		ascii = cadena.charAt(i);
-    		System.out.println(ascii);
+    		binario = Integer.toBinaryString(ascii);
+    		System.out.println(ascii + " = " + binario);
     	}
     	
     }//conversor
