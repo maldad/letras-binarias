ec6853afab0a1f7fa52eac3efa2f45229e44f1e2
maldad <canond_7@hotmail.com>
2015-05-17 00:59:20 -0500

    abecedario a ASCII

--- LetraBinaria.java
+++ LetraBinaria.java
@@ -1,5 +1,35 @@
 public class LetraBinaria{
 	
+	/*public static void main (String args[]){
+      String letras ="oso", n;
+      int x=0;
+      for (int i=0; i<letras.length(); i++){
+      x=letras.charAt(i);
+      n=Integer.toBinaryString(x);
+      System.out.println(letras.charAt(i));
+      System.out.println(x);
+      System.out.println(n+"\n");
+      }
+    }//main */
+
+    String cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int ascii;
+
+    public void conversor(){
+    	
+    	for(int i = 0; i < cadena.length(); i++){
+    		ascii = cadena.charAt(i);
+    		System.out.println(ascii);
+    	}
+    	
+    }//conversor
+
+    public static void main(String [] args){
+
+    	LetraBinaria lb = new LetraBinaria();
+    	lb.conversor();
+
+    }//main
+      
 	
-	
-}
+}//end class
